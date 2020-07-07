b87c758f586bb3db323d8a0ca168c14f6a2c0b8b
anu13 <anu13@DESKTOP-P2MFD5I.myfiosgateway.com>
2020-07-07 12:56:45 -0400

    another issue

--- FCRMBDDProject/src/main/java/com/testrunner/TestRunner.java
+++ FCRMBDDProject/src/main/java/com/testrunner/TestRunner.java
@@ -3,7 +3,13 @@ package com.testrunner;
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
-@CucumberOptions(features = "src/test/resources/Features/login.feature", glue =
+@CucumberOptions(features = "src/test/resources/Features/login.feature", plugin =
+{
+ "pretty",
+ "html:reports/cucumber-html-report",
+ "junit:reports/cucumber-html-report/xml",
+ "com.cucumber.listener.ExtentCucumberFormatter:reports/Extentreports/Extentreport.html"
+}, glue =
 {
  "com.stepdefinition"
 }, monochrome = true, dryRun = false)
